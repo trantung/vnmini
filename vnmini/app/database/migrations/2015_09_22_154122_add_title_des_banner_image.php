02529d665084ca5a9c80be6092b39b2c253dfc50
Tran Thanh Tung <tranthanhtung@Trans-MacBook-Pro.local>
2015-09-22 22:43:48 +0700

    add title and description into bannerimages

---
+++ vnmini/app/database/migrations/2015_09_22_154122_add_title_des_banner_image.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTitleDesBannerImage extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('banner_images', function(Blueprint $table) {
+            $table->string('title', 256)->after('image_url')->nullable();
+            $table->string('description', 256)->nullable();
+        });		
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        $table->dropColumn('title');
+        $table->dropColumn('description');
+	}
+
+}
