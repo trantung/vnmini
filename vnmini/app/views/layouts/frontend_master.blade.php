110db8e0a441fe08c8de4838f48792d48711374f
dxnm <2711sq@gmail.com>
2016-01-06 11:40:31 +0700

    fix

--- vnmini/app/views/layouts/frontend_master.blade.php
+++ vnmini/app/views/layouts/frontend_master.blade.php
@@ -8,7 +8,7 @@
         <!-- <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
         <title>{{ $info->name }}</title>
-        <link rel="shortcut icon" href="{{ asset('img/logo_short.png') }}" />
+        <link rel="shortcut icon" href="{{ asset('img/favicon.jpg') }}" />
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <meta name="description" content="{{ $des->description }}">
         <meta name="keyword" content="{{ $des->keyword }}">
